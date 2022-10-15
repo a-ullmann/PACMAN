3e1071e5f7b0e9283481246a39380276387c1161
Alex Ullmann <aullmann3@gmail.com>
2022-10-15 19:18:36 +0200

    collision detection added

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ day 1:
 completed a thorough plan of action consisting of an overview for HTML, CSS, JS
 HTML & CSS: creating a base grid layout for the playing field
 JS: pseudocode the most important functions, such as starting the game, movements, collisions, score point system
+
+day 2: 
+finalized the map by sketching it out and defining each wall. successfully implemented collision detection for walls and ghosts
--- scripts/app.js
+++ scripts/app.js
@@ -1,111 +1,169 @@
 function init () {
-
+  
+  // Elements
   const grid = document.querySelector('.grid')
-
-  const width = 10
-  const height = 15
+  const width = 20
+  const height = 22
   const cellCount = width * height
   const cells = []
+  const startingPositionPac = 115
+  const startingPositionGhostY = 45
+  const startingPositionGhostB = 46
   
-  const startingPositionPac = 125
+  // Variables
   let currentPositionPac = startingPositionPac
-  const startingPositionGhost = 45
-  let currentPositionGhost = startingPositionGhost
-
+  let currentPositionGhostY = startingPositionGhostY
+  let currentPositionGhostB = startingPositionGhostB
   let timer
-
+  
+  
+  // Execution
   function createGrid(){
     for (let i = 0; i < cellCount; i++){
       const cell = document.createElement('div')
       cell.innerHTML = i
-      cell.dataset.index = i
+      cell.classList.add(i)
       grid.appendChild(cell)
       cells.push(cell)
     }
+    
     addPacman(startingPositionPac)
-    addGhost(startingPositionGhost)
+    addGhost(startingPositionGhostY)
+    addGhost(startingPositionGhostB)
   }
   createGrid()
   
-  // function createWalls(){
-  // wall.classList.add('wall')
-  //   cells.classList.add('wall')
-  // }
-  // createWalls()
-
-
+  
+  const walls = [cells[0], cells[1], cells[2], cells[3], cells[4], cells[5], cells[6], cells[7], cells[8], cells[9], cells[10], cells[11], cells[12], cells[13], cells[14], cells[15], cells[16], cells[17], cells[18], cells[19],
+    cells[10], cells[20], cells[30], cells[40], cells[50], cells[60], cells[70], cells[80], cells[90], cells[100], cells[110], cells[120], cells[130], cells[140],
+    cells[141], cells[142], cells[143], cells[144], cells[145], cells[146], cells[147], cells[148], cells[149], 
+    cells[19], cells[29], cells[39], cells[49], cells[59], cells[69], cells[79], cells[89], cells[99], cells[109], cells[119], cells[129], cells[139],
+    cells[122], cells[123], cells[124], cells[125], cells[126], cells[127]]
+  
+  function createWalls(){
+    walls.forEach(wall => wall.classList.add('wall'))
+  }
+  createWalls()
+    
+  
   function addPacman(position) {
     cells[position].classList.add('pacman')
   }
-
+  
   function removePacman(position){
     cells[position].classList.remove('pacman')
   }
 
+  
   function movePacman(event){
     const key = event.keyCode
-    console.log(key)
     const up = 38
     const down = 40
     const left = 37
     const right = 39
     removePacman(currentPositionPac)
     
-    if (key === right && currentPositionPac % width !== width - 1) {
-      console.log('right')
+    if (key === right && currentPositionPac % width !== width - 1 && doesContainWallRight() === false) {
       currentPositionPac++
-    } else if (key === left && currentPositionPac % width !== 0) {
-      console.log('left')
+      ghostKillPacman()
+    } else if (key === left && currentPositionPac % width !== 0 && doesContainWallLeft() === false) {
       currentPositionPac--
-    } else if (key === up && currentPositionPac >= width) {
-      console.log('up')
+      ghostKillPacman()
+    } else if (key === up && currentPositionPac >= width && doesContainWallUp() === false) {
       currentPositionPac -= width
-    } else if (key === down && currentPositionPac < cellCount - width){
-      console.log('down')
+      ghostKillPacman()
+    } else if (key === down && currentPositionPac < cellCount - width && doesContainWallDown() === false){
       currentPositionPac += width
+      ghostKillPacman()
     }
     addPacman(currentPositionPac)
   }
+  
+  
+  function doesContainWallRight() {
+    const nextPositionRight = currentPositionPac + 1
+    if (cells[nextPositionRight].classList.contains('wall')) {
+      return true
+    } else {
+      return false
+    } 
+  }
+  
+  function doesContainWallLeft() {
+    const nextPositionLeft = currentPositionPac - 1
+    if (cells[nextPositionLeft].classList.contains('wall')) {
+      return true
+    } else {
+      return false
+    }
+  } 
+  
+  function doesContainWallUp() {
+    const nextPositionUp = currentPositionPac - width
+    if (cells[nextPositionUp].classList.contains('wall')) {
+      return true
+    } else {
+      return false
+    }
+  }
+  
+  function doesContainWallDown() {
+    const nextPositionDown = currentPositionPac + width
+    if (cells[nextPositionDown].classList.contains('wall')) {
+      return true
+    } else {
+      return false
+    }
+  }
+    
+  function newCurrentPositionPac() {
+    removePacman(currentPositionPac)
+    currentPositionPac = 170
+    addPacman(currentPositionPac)
+  }
 
-  document.addEventListener('keydown', movePacman)
+  function ghostKillPacman() {
+    if (currentPositionGhostY === currentPositionPac || currentPositionGhostB === currentPositionPac){
+      console.log('hi')
+      newCurrentPositionPac()
+    }
+  }
 
 
   function addGhost(position) {
     cells[position].classList.add('ghost')
   }
-
+  
   function removeGhost(position) {
     cells[position].classList.remove('ghost')
   }
 
-  function moveGhost(){
+  function moveGhostY(){
     timer = setInterval(() => {
-      if (currentPositionGhost < cellCount - width){
-        removeGhost(currentPositionGhost)
-        console.log('ghost is moving down')
-        currentPositionGhost += width
-        addGhost(currentPositionGhost)
+      if (currentPositionGhostY < cellCount - width){
+        removeGhost(currentPositionGhostY)
+        currentPositionGhostY += width
+        addGhost(currentPositionGhostY)
       }
-    }, 1000)
+      ghostKillPacman()
+    }, 400)
   }
-
-  moveGhost()
-
-
-
-
-
-
-
-
-
-
-
-
+  function moveGhostB(){
+    timer = setInterval(() => {
+      if (currentPositionGhostB < cellCount - width){
+        removeGhost(currentPositionGhostB)
+        currentPositionGhostB++
+        addGhost(currentPositionGhostB)
+      }
+      ghostKillPacman()
+    }, 400)
+  }
+  moveGhostY()
+  moveGhostB()
 
 
-
-
+  // Events
+  document.addEventListener('keydown', movePacman)
 
 
 
@@ -118,6 +176,36 @@ function init () {
 
 
 
+
+  // Notes
+
+  // 1 = wall
+  // 0 = path
+
+
+  const map = [
+    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+    [1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1],
+    [1,0,1,1,0,1,1,1,0,1,0,1,1,1,0,1,1,0,1],
+    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,1,1,0,1,0,1,1,1,1,1,0,1,0,1,1,0,1],
+    [1,0,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0,0,1],
+    [1,1,1,1,0,1,1,1,0,1,0,1,1,1,0,1,1,1,1],
+    [1,1,1,1,0,1,0,0,0,0,0,0,0,1,0,1,1,1,1],
+    [1,1,1,1,0,1,0,1,1,1,1,1,0,1,0,1,1,1,1],
+    [0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0],
+    [1,1,1,1,0,1,0,1,1,1,1,1,0,1,0,1,1,1,1],
+    [1,1,1,1,0,1,0,0,0,0,0,0,0,1,0,1,1,1,1],
+    [1,1,1,1,0,1,0,1,1,1,1,1,0,1,0,1,1,1,1],
+    [1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1],
+    [1,0,1,1,0,1,1,1,0,1,0,1,1,1,0,1,1,0,1],
+    [1,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,1],
+    [1,1,0,1,0,1,0,1,1,1,1,1,0,1,0,1,0,1,1],
+    [1,0,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0,0,1],
+    [1,0,1,1,1,1,1,1,0,1,0,1,1,1,1,1,1,0,1],
+    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+  ]
 
 
 
